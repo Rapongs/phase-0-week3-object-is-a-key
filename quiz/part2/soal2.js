67d44efa1c8677798d542a5eef1816cd56fc7035
Rapongs <gnopgnopar@gmail.com>
2026-02-18 15:49:34 +0700

    pengerjaan quiz part 2

---
+++ quiz/part2/soal2.js
@@ -0,0 +1,23 @@
+//DILARANG MENGGUNAKAN METHOD SORT, PELAJARI ALGORITMA SORTING YANG ADA DI GOOGLE
+//saran sih pake bubblesort walau tidak efisien tapi bagus buat belajar sorting
+function urutkanAbjad(str) {
+  // you can only write your code here!
+    var arr = str.split('');
+    for (var i = 0; i < arr.length - 1; i++) {
+        for (var j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr.join('');
+}
+
+// TEST CASES
+console.log(urutkanAbjad('hello')); // 'ehllo'
+console.log(urutkanAbjad('truncate')); // 'acenrttu'
+console.log(urutkanAbjad('developer')); // 'deeeloprv'
+console.log(urutkanAbjad('software')); // 'aeforstw'
+console.log(urutkanAbjad('aegis')); // 'aegis'
